f60db91e13c2351ca171b5251be24f4c681aa6c7
eceranaozyurt <eceranaozyrt@gmail.com>
2026-01-15 15:39:30 +0300

    Fix app.js syntax

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 
-// CORS ayarları Vercel için
+// CORS ayarları
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
